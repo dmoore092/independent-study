8541d3967f90fc9e5a7e40fb4638431f417eb5f4
Dale Moore <dwmdis@rit.edu>
2020-09-03 15:51:18 -0400

    adding /build

---
+++ build/precache-manifest.7ba1675bdbd617f1b103a6c83e59f4f5.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c57641f5c4321deb0b88d9e35f3863dd",
+    "url": "/independent-study/index.html"
+  },
+  {
+    "revision": "a56ad4f28e1212718d62",
+    "url": "/independent-study/static/css/3.6f840912.chunk.css"
+  },
+  {
+    "revision": "2ef3c2f08312efdd3659",
+    "url": "/independent-study/static/css/4.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "818fc75bd6917220644e",
+    "url": "/independent-study/static/css/5.6f840912.chunk.css"
+  },
+  {
+    "revision": "02f25ec443319a4e06e5",
+    "url": "/independent-study/static/css/main.c13fbabc.chunk.css"
+  },
+  {
+    "revision": "f8fe78439b0230471b55",
+    "url": "/independent-study/static/js/0.cb405f0c.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/independent-study/static/js/0.cb405f0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51746307d9f89baf28ae",
+    "url": "/independent-study/static/js/10.97c12873.chunk.js"
+  },
+  {
+    "revision": "ba1caaf53a125b41844b",
+    "url": "/independent-study/static/js/11.d77e945e.chunk.js"
+  },
+  {
+    "revision": "49352a4edbc701d0f9a7",
+    "url": "/independent-study/static/js/12.2f893374.chunk.js"
+  },
+  {
+    "revision": "2eabab01b5ae242028fd",
+    "url": "/independent-study/static/js/13.d3f9d5a3.chunk.js"
+  },
+  {
+    "revision": "d2c7f5238f3d1f6397d7",
+    "url": "/independent-study/static/js/14.8c7db48f.chunk.js"
+  },
+  {
+    "revision": "341b0706266ec96fdc6e",
+    "url": "/independent-study/static/js/15.f032aead.chunk.js"
+  },
+  {
+    "revision": "9e3dc58ba9dc25d6e1eb",
+    "url": "/independent-study/static/js/16.4cc67056.chunk.js"
+  },
+  {
+    "revision": "5e9e9366b48be1c1d82e",
+    "url": "/independent-study/static/js/17.9d3ae22e.chunk.js"
+  },
+  {
+    "revision": "a56ad4f28e1212718d62",
+    "url": "/independent-study/static/js/3.70a54b8d.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/independent-study/static/js/3.70a54b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ef3c2f08312efdd3659",
+    "url": "/independent-study/static/js/4.218400c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/independent-study/static/js/4.218400c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "818fc75bd6917220644e",
+    "url": "/independent-study/static/js/5.800b639c.chunk.js"
+  },
+  {
+    "revision": "c360eff85ca112d098c080b19dff9b78",
+    "url": "/independent-study/static/js/5.800b639c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3d23826dc9870e6491e",
+    "url": "/independent-study/static/js/6.f3ba01d2.chunk.js"
+  },
+  {
+    "revision": "cbf94e527e1920dc9be6",
+    "url": "/independent-study/static/js/7.aa9701a5.chunk.js"
+  },
+  {
+    "revision": "db59832d6ea7ce043d25",
+    "url": "/independent-study/static/js/8.0602a149.chunk.js"
+  },
+  {
+    "revision": "873d72ca7523a77efb82",
+    "url": "/independent-study/static/js/9.a8690f17.chunk.js"
+  },
+  {
+    "revision": "02f25ec443319a4e06e5",
+    "url": "/independent-study/static/js/main.daa31ab0.chunk.js"
+  },
+  {
+    "revision": "823dec0fc42c5cf85709",
+    "url": "/independent-study/static/js/runtime-main.9602d013.js"
+  },
+  {
+    "revision": "bb13993f0f58556be6fa0c2633a8af05",
+    "url": "/independent-study/static/media/background.bb13993f.jpg"
+  },
+  {
+    "revision": "eaa1452fa89ecad3d91f1c92fe9b71b5",
+    "url": "/independent-study/static/media/preloader4.eaa1452f.png"
+  }
+]);
