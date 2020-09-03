1038c0c84f4e2d5cd620d323c5ab23c434fa3cf8
Dale Moore <dwmdis@rit.edu>
2020-09-03 15:35:20 -0400

    heroku buildpack

--- public/proxy.php
+++ public/proxy.php
@@ -5,6 +5,7 @@ error_reporting(0);
 // ini_set('display_startup_errors', 1);
 header("Access-Control-Allow-Origin: *");
 
+
 //sets the base of the url we want to hit
 define ('HOSTNAME', 'http://www.ist.rit.edu/api');
 
